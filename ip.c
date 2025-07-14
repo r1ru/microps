b9af6ffdc8346496060c4dd15888219941808cb8
r1ru <riruoda@gmail.com>
2025-07-14 13:04:35 +0900

    Finish step6: Verify IP datagrams

--- ip.c
+++ ip.c
@@ -1,10 +1,128 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
 #include "net.h"
 #include "util.h"
 #include "ip.h"
 
+// IP header.
+struct ip_hdr {
+    // Version, and header length.
+    uint8_t vhl;
+    // Type of service.
+    uint8_t tos;
+    // Total Length
+    uint16_t total;
+    uint16_t id;
+    // Flag, and flagment offset.
+    uint16_t offset; 
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t sum;
+    ip_addr_t src;
+    ip_addr_t dst;
+    uint8_t options[];
+};
+
+// Converts a printable text into an ip address.
+int ip_addr_pton(const char *p, ip_addr_t *n) {
+    char *sp, *ep;
+    int idx;
+    long ret;
+
+    sp = (char *)p;
+    for (idx = 0; idx < 4; idx++) {
+        ret = strtol(sp, &ep, 10);
+        if (ret < 0 || ret > 255) {
+            return -1;
+        }
+        if (ep == sp) {
+            return -1;
+        }
+        if ((idx == 3 && *ep != '\0') || (idx != 3 && *ep != '.')) {
+            return -1;
+        }
+        ((uint8_t *)n)[idx] = ret;
+        sp = ep + 1;
+    }
+    return 0;
+}
+
+// Converts an ip address into a printable text.
+char * ip_addr_ntop(ip_addr_t n, char *p, size_t size) {
+    uint8_t *u8;
+
+    u8 = (uint8_t *)&n;
+    snprintf(p, size, "%d.%d.%d.%d", u8[0], u8[1], u8[2], u8[3]);
+    return p;
+}
+
+// Dumps a IP packet.
+void ip_dump(const uint8_t *data, size_t len) {
+    struct ip_hdr *hdr;
+    uint8_t v, hl, hlen;
+    uint16_t total, offset;
+    char addr[IP_ADDR_STR_LEN];
+
+    flockfile(stderr);
+    hdr = (struct ip_hdr *)data;
+    v = (hdr->vhl & 0xf0) >> 4;
+    hl = hdr->vhl & 0x0f;
+    hlen = hl << 2;
+    fprintf(stderr, "        vhl: 0x%02x [v: %u, hl: %u (%u)]\n", hdr->vhl, v, hl, hlen);
+    fprintf(stderr, "        tos: 0x%02x\n", hdr->tos);
+    total = ntoh16(hdr->total);
+    fprintf(stderr, "      total: %u (payload: %u)\n", total, total - hlen);
+    fprintf(stderr, "         id: %u\n", ntoh16(hdr->id));
+    offset = ntoh16(hdr->offset);
+    fprintf(stderr, "     offset: 0x%04x [flags=%x, offset=%u]\n", offset, (offset & 0xe000) >> 13, offset & 0x1fff);
+    fprintf(stderr, "        ttl: %u\n", hdr->ttl);
+    fprintf(stderr, "   protocol: %u\n", hdr->protocol);
+    fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+    fprintf(stderr, "        src: %s\n", ip_addr_ntop(hdr->src, addr, sizeof(addr)));
+    fprintf(stderr, "        dst: %s\n", ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);    
+}
+
 static void ip_input(const uint8_t *data, size_t len, struct net_device *dev) {
-    debugf("dev = %s, len=%zu", dev->name, len);
-    debugdump(data, len);    
+    struct ip_hdr *hdr;
+    uint8_t v;
+    uint16_t hlen, total, offset;
+
+    if (len < IP_HDR_SIZE_MIN) {
+        errorf("too short");
+        return;
+    }
+    hdr = (struct ip_hdr *)data;
+    v = (hdr->vhl & 0xf0) >> 4;
+    if (v != IP_VERSION_IPV4) {
+        errorf("unsupported version");
+        return;
+    }
+    hlen = (hdr->vhl & 0x0f) << 2;
+    if (len < hlen) {
+        errorf("too short");
+        return;
+    }
+    total = ntoh16(hdr->total);
+    if (len < total) {
+        errorf("too short");
+        return;
+    }
+    if (cksum16((uint16_t *)data, hlen, 0) != 0) {
+        errorf("wrong checksum");
+        return;
+    }
+    offset = ntoh16(hdr->offset);
+    if (offset & 0x2000 || offset & 0x1fff) {
+        errorf("flagments are not supported");
+        return;
+    }
+    debugf("dev=%s, protocol=%u, total=%u", dev->name, hdr->protocol, total);
+    ip_dump(data, total);
 }
 
 int ip_init(void) {
--- ip.h
+++ ip.h
@@ -3,6 +3,24 @@
 
 #include "net.h"
 
+#define IP_VERSION_IPV4 4
+
+#define IP_HDR_SIZE_MIN 20
+#define IP_HDR_SIZE_MAX 60
+
+#define IP_TOTAL_SIZE_MAX UINT16_MAX /* maximum value of uint16 */
+#define IP_PAYLOAD_SIZE_MAX (IP_TOTAL_SIZE_MAX - IP_HDR_SIZE_MIN)
+
+#define IP_ADDR_LEN 4
+#define IP_ADDR_STR_LEN 16 /* "ddd.ddd.ddd.ddd\0" */
+
+typedef uint32_t ip_addr_t;
+
+extern const ip_addr_t IP_ADDR_ANY;
+extern const ip_addr_t IP_ADDR_BROADCAST;
+
+int ip_addr_pton(const char *p, ip_addr_t *n);
+char *ip_addr_ntop(ip_addr_t n, char *p, size_t size);
 int ip_init(void);
 
 #endif
